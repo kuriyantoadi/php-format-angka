f416140682fca9f0098e943cf92e7a7d0d923cf7
Kuriyanto Adi Saputro <melatikuning212@gmail.com>
2024-01-07 23:45:15 +0700

    Add files via upload

---
+++ fomat-angka.php
@@ -0,0 +1,8 @@
+<?php
+// Contoh data
+$jumlah_angka = 1234567.89;
+
+// Menampilkan data dengan format angka tanpa desimal
+echo "Jumlah Angka: " . number_format($jumlah_angka, 0, ',', '.'); // Format: 1.234.568
+?>
+
